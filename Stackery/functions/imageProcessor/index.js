55eb046cddafb40d07e21b11d65db1b6cbb774b1
tobyfee@stackery.io <tobyfee@stackery.io>
2018-05-10 18:18:28 +0000

    Modified 2 nodes:
    Updated function node imageProcessor
    Updated custom node lambda-on-deploy

--- Stackery/functions/imageProcessor/index.js
+++ Stackery/functions/imageProcessor/index.js
@@ -1,103 +1,27 @@
 const AWS = require('aws-sdk');
-const fs = require('fs')
-const fileName = 'hello.html'
+const fs = require('fs');
+const fileName = 'hello.html';
+const cfnCR = require('cfn-custom-resource');
+
 
 const s3 = new AWS.S3();
 
-module.exports = function handler(event, context, callback) {
-  console.log(event);
+module.exports = async event => {
+  console.dir(event);
   const ports = JSON.parse(process.env.STACKERY_PORTS)
-  fs.readFile(`./${fileName}`, 'utf8', function(err, data) {
+  let data = fs.readFileSync(`./${fileName}`, 'utf8')
+  let params = {
+    Body: data,
+    Key: `${fileName}`,
+    Bucket: ports[0][0].bucket
+  };
+  s3.putObject(params, async (err, data) => {
     if (err) {
-      return console.log(err);
+      console.log(err);
+      await cfnCR.sendFailure(err.message, event);
+    } else {
+      console.dir(data);
+      await cfnCR.sendSuccess('s3-copy', {}, event);
     }
-    let params = {
-      Body: data,
-      Key: `${fileName}`,
-      Bucket: ports[0][0].bucket
-    };
-    console.log(data);
-    s3.putObject(params, (err, data) => {
-      if (err) {
-        console.log(err);
-        callback(null, {});
-      } else {
-        console.dir(data);
-        callback(null, {});
-
-      }
-    })
-  });
-  // console.log(ports[0][0].bucket)
-  // console.dir(ports[0])
-  //
-  // let record = event.Records[0];
-  // if (record.eventName !== 'ObjectCreated:Put') {
-  //   return;
-  // }
-  // let sourceBucket = record.s3.bucket.name;
-  // let objectKey = record.s3.object.key;
-  //
-  // // Only operate on image files
-  // // For simplicity in this guide, we'll only operate on '.jpg' files
-  // let result = objectKey.match(/(.*)\.jpg$/);
-  // if (!result) {
-  //   return;
-  // }
-  // let imageBuffer;
-  // let params = {
-  //   Key: objectKey,
-  //   Bucket: sourceBucket
-  // };
-  //
-  // // Retrieve image from ObjectStore "Uploaded Images"
-  // s3.getObject(params).promise()
-  //   .then((data) => {
-  //     return new Promise((resolve, reject) => {
-  //       imageBuffer = data.Body;
-  //
-  //       // imageMagick create a 200x200 thumbnail
-  //       gm(imageBuffer)
-  //         .resize(200, 200)
-  //         .stream((err, stdout, stderr) => {
-  //           let chunks = [];
-  //
-  //           stdout.on('data', (chunk) => {
-  //             chunks.push(chunk);
-  //           });
-  //
-  //           stdout.on('end', () => {
-  //             resolve(Buffer.concat(chunks));
-  //           });
-  //
-  //           if (err) {
-  //             console.log(`Error resizing image: ${err}`);
-  //             reject(err);
-  //           }
-  //
-  //           stderr.on('data', function (data) {
-  //             console.log(`Error resizing image: ${data}`);
-  //             reject(new Error('Error resizing image'));
-  //           });
-  //         });
-  //     });
-  //   })
-  //   .then((outputBuffer) => {
-  //     // Store generated thumbnail to Object Store "Processed Images"
-  //     // We look up the S3 bucket using the STACKERY_PORTS env var
-  //     const ports = JSON.parse(process.env.STACKERY_PORTS);
-  //     let params = {
-  //       Body: outputBuffer.toString('binary'),
-  //       Key: `200x200-${objectKey}`,
-  //       Bucket: ports[0][0].bucket
-  //     };
-  //     return s3.putObject(params).promise();
-  //   })
-  //   .then((response) => {
-  //     // Done!
-  //     callback(null, {});
-  //   })
-  //   .catch((error) => {
-  //     callback(error);
-  //   });
-};
+  })
+}
